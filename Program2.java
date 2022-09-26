9285db9365223e36cf86c7cb07dd55071a3b09c2
SunnyKharwar <91607603+Sunny8250@users.noreply.github.com>
2022-09-26 15:34:27 +0530

    Update Program2.java

--- Program2.java
+++ Program2.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class Program_2 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the numbers of odd numbers to be printed");
+		int a = scan.nextInt();
+		while( i <= a) 
+		{
+			System.out.print(2 * i - 1 + " 10");
+			i++;
+		}
+	}
+}
